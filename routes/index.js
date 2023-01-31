daf8ed10ef3eb3bf37e554bb949fc2b6176df20e
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-01-31 10:27:47 +0700

    Update to make it look more scifi

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ module.exports = (req, res) => {
     <html>
       <head>
         <link rel="stylesheet" type="text/css" href="/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=SciFi&display=swap" rel="stylesheet">
       </head>
       <body>
+        <div id="title">SWN Generator</div>
         <div style="display: flex; justify-content: center;">
             <button id="beast-button">Beast</button>
             <button id="standard-npc-button">NPC</button>
