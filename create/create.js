e4b44e7219d4f5dd0f6c0c3cfc188f380c515a18
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-02-01 19:39:11 +0700

    Cycles through each stage of the character generation

--- create/create.js
+++ create/create.js
@@ -1,5 +1,12 @@
+const endpoints = [
+  { endpoint: "stat-block", display: "stat-block-display" },
+  { endpoint: "origin", display: "origin-display" }
+];
+
+let currentEndpoint = 0;
+
 function handleStatBlockClick(event) {
-  fetch(`https://swn-generate.herokuapp.com/create/pc/stat-block`, { method: 'GET' })
+  fetch(`https://swn-generate.herokuapp.com/create/pc/${endpoints[currentEndpoint].endpoint}`, { method: 'GET' })
     .then(response => {
       if (!response.ok) {
         throw new Error(response.statusText)
@@ -7,11 +14,19 @@ function handleStatBlockClick(event) {
       return response.text()
     })
     .then(text => {
-      document.getElementById('stat-block-display').innerHTML = text.replace(/\n/g, '<br>').replace("'", "&#39;")
+      document.getElementById(endpoints[currentEndpoint].display).innerHTML = text.replace(/\n/g, '<br>').replace("'", "&#39;")
+      currentEndpoint++;
+      if (currentEndpoint >= endpoints.length) {
+          document.getElementById("primary-button").style.display = "none";
+          const parentDiv = document.getElementById("primary-button").parentNode;
+          parentDiv.parentNode.removeChild(parentDiv);
+      } else {
+        document.getElementById("primary-button").innerHTML = `Create ${endpoints[currentEndpoint].endpoint.replace("-", " ")}`;
+      }
     })
     .catch(error => {
       console.error('Error fetching data:', error)
     })
 }
 
-document.getElementById("stat-block-button").addEventListener('click', handleStatBlockClick)
+document.getElementById("primary-button").addEventListener('click', handleStatBlockClick)
