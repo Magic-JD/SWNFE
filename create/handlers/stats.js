327583d736e8fba815cf96f57200e452c9326a1b
Magic-JD <mag1ctr1cks@hotmail.co.uk>
2023-02-07 10:26:27 +0700

    Update the front end to use stats correctly

--- create/handlers/stats.js
+++ create/handlers/stats.js
@@ -1,21 +1,22 @@
 import { handleGenerateClickRequest } from "./requests/requests.js";
 import { getDisplay } from "../display/display.js";
+import { getPC, Stat } from "../pc/pc.js";
 
-let statProperties = null;
 const pickValues = ["14 [+1]", "12 [0]", "11 [0]", "10 [0]", "9 [0]", "7 [-1]"]
 let pickValueIndex = 0
 let display = getDisplay();
+let pc = getPC()
 
 export function initStatNames() {
   handleGenerateClickRequest("stat-block").then(text => {
-    statProperties = JSON.parse(text).properties.sort((a, b) => a.priority - b.priority);
+    pc.stats = JSON.parse(text).properties.map(p => new Stat(p.name, p.value, p.mod, p.priority)).sort((a, b) => a.priority - b.priority);
   })
 }
 
 export function handleClickPickStats(event) {
   display.replaceText(createStatApplyString())
   display.clear()
-  statProperties.forEach(p => {
+  pc.stats.forEach(p => {
     event.target.parentNode.append(createStatAddButton(p.name.toLowerCase, p.name, pickStatListener));
   })
 }
@@ -40,7 +41,7 @@ function pickStatListener(event) {
 function sortStats(string, outOfIndex) {
   let list = string.split('<br>')
   list.shift()
-  string = list.sort((a, b) => statProperties.find(e2 => e2.name == a.split(":")[0]).priority - statProperties.find(e1 => e1.name == b.split(":")[0]).priority).join('<br>')
+  string = list.sort((a, b) => pc.stats.find(e2 => e2.name == a.split(":")[0]).priority - pc.stats.find(e1 => e1.name == b.split(":")[0]).priority).join('<br>')
   if (outOfIndex) {
     return string
   } else {
@@ -58,10 +59,10 @@ function createStatAddButton(id, details, listener) {
 
 export function handleGenerateClickStats(event) {
   let details = ""
-  statProperties.forEach(p => details += statsToString(p));
+  pc.stats.forEach(p => details += statsToString(p));
   display.addText(`Apply ${pickValues[0]} to chosen stat<br>${details}`)
   display.clear()
-  statProperties.forEach(p => {
+  pc.stats.forEach(p => {
     event.target.parentNode.append(createStatAddButton(p.name.toLowerCase, p.name, pickStatClickListener));
   })
 }
